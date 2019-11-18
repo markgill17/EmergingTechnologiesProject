49a333126d97703a9054c25596edd0baff38b5c1
markgill <G00348787@gmit.ie>
2019-11-18 18:43:54 +0000

    added app.py

---
+++ webapp/app.py
@@ -0,0 +1,26 @@
+import flask as fl
+import base64
+import numpy as np
+
+app = fl.Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return app.send_static_file('webpage.html')
+
+
+@app.route('/predictDigit', methods=['POST'])
+def convertImage():
+    # get the image from the request
+    encodedImage = fl.request.values[('imgBase64')]
+
+    # decode the dataURL
+    # remove the added part of the url start from the 22 index of the image array
+    decodedImage = base64.b64decode(encodedImage[22:])
+
+    # save the image
+    with open('image.png', 'wb') as f:
+        f.write(decodedImage)
+
+    return encodedImage
